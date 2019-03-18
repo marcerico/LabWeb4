f23b5223eba36b49c927e955bdd81576d9c8e3f5
Marcela Rico <marce.rr@hotmail.com>
2019-03-18 13:13:40 -0600

    Changes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 const credentials = require('./credentials.js')
 const request = require('request')
 
-const mapBoxUrl = `https : // api.mapbox.com/geocoding/v5/mapbox.places/`
-const darkSkyUrl = `https: //api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}`;
-
 const readline = require('readline')
 
 const rl = readline.createInterface({
@@ -16,30 +13,31 @@ rl.question('Cual es tu ciudad? ', function(cityName) {
   rl.close();
 });
 
-function getWeather(longitude,latitude){
-  request({url : darkSkyUrl,json : true},function(error,response){
-    const data={
-    temperature:  body.currently.temperature,
-    precip_probability: body.currently.precipProbability,
-    summary:  body.currently.summary
-    //response: `Hoy :  ${summary}. El clima se encuentra en :  ${temp} gradosC. Hay la probabilidad de que llueva de un ${precipProb}%`
-  }
-    console.log(data)
+const mapBoxUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/${credentials.MAPBOX_TOKEN}`
+const darkSkyUrl = `https: //api.darksky.net/forecast/${credentials.DARK_SKY_SECRET_KEY}`;
+
+function getCoordinates(cityName){
+  request({url: mapBoxUrl , json: true},function(error,response){
+      const data = response.body.features[0]
+      getWeather(data.center[0],data.center[1])
   })
 }
 
-function getCoordinates(cityName){
-  request({url: mapBoxUrl,json: true},function(error,response){
-    if(error){
-      console.log(error,undefined)
-    }
-    else{
-      const data={
-      longitude : body.features[0].center[0],
-      latitude : body.features[0].center[1]
-    }
-      getWeather(longitude,latitude)
 
+function getWeather(longitude,latitude){
+  request({url : darkSkyUrl , json : true},function(error,response){
+    const data = response.body.currently
+    const weather ={
+    day : data.summary,
+    temperature:  data.temperature,
+    precipitation_prob: data.precipProbability
     }
+    const info = `Hoy ${weather.day}. El clima está a ${weather.temperature}°C. La probabilidad de que llueva es de ${weather.precipitation_prob}%`
+    console.log(info)
   })
+}
+
+module.exports = {
+  getCoordinates: getCoordinates,
+  getWeather : getWeather
 }
